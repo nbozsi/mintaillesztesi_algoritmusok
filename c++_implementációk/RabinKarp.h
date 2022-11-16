fc74532c9362f16b87b5c25e05c8ceda8a2b7c70
nbozsi <nbozsi4@gmail.com>
2022-11-16 12:40:39 +0100

    hibák javítása

--- c++_implementációk/RabinKarp.h
+++ c++_implementációk/RabinKarp.h
@@ -5,7 +5,7 @@ map<char, int> charvalue(string szoveg)
 {
     map<char, int> E;
     int next = 1;
-    std::pair<std::map<char, int>::iterator, bool> ret;
+    pair<map<char, int>::iterator, bool> ret;
     for (int i = 0; i < szoveg.length(); i++)
     {
         ret = E.insert({szoveg[i], next});
@@ -18,30 +18,33 @@ map<char, int> charvalue(string szoveg)
 }
 int RabinKarp(string minta, string szoveg, map<char, int> E)
 {
-    int alap = E.size() + 2; // szamrendszer alapszama
+    int alap = E.size() + 1; // szamrendszer alapszama
     int M = 1000000007;      // ez egy nagyon nagy prímszám
     int maxh = 1;
-    int mintahash, vizsgalthash;
+    int mintahash = 0;
+    int vizsgalthash = 0;
     for (int i = 0; i < minta.length(); i++)
     {
         mintahash = (mintahash * alap + E[minta[i]]) % M;        // minta hashének kiszámítása
         vizsgalthash = (vizsgalthash * alap + E[szoveg[i]]) % M; // szoveg első substringének kiszámítása
-        maxh = (maxh * alap) % M;                                // legnagyobb helyiérték kiszámítása
+        maxh = (maxh * alap);                                    // legnagyobb helyiérték kiszámítása
     }
-    cout << mintahash << endl;
+    maxh = (maxh / alap) % M;
+    // cout << mintahash << " " << maxh << endl;
     if (mintahash == vizsgalthash)
     {
         return 0; // azaz a szoveg a mintával kezdődik
     }
     for (int i = 1; i <= szoveg.length() - minta.length(); i++)
     {
-        cout << vizsgalthash << " ";
-        vizsgalthash = (vizsgalthash - szoveg[i - 1] * maxh + M) % M;          // betű eltávolítása az elejéről (hozzáadunk M-t hogy pozitív maradék legyen)
-        vizsgalthash = (vizsgalthash * alap + szoveg[i + minta.length()]) % M; // betű hozzáadása a végéhez
+        // cout << vizsgalthash << " ";
+        vizsgalthash = (vizsgalthash - E[szoveg[i - 1]] * maxh) % M;                  // betű eltávolítása az elejéről (hozzáadunk M-t hogy pozitív maradék legyen)
+        vizsgalthash = (vizsgalthash * alap + E[szoveg[i + minta.length() - 1]]) % M; // betű hozzáadása a végéhez
         if (vizsgalthash == mintahash)
         {
             return i;
         }
     }
+    // cout << vizsgalthash;
     return -1;
 }
