759b80194672b91583a1fc9945a060208c285972
nbozsi <nbozsi4@gmail.com>
2023-05-30 18:56:07 +0200

    map -> vector ascii

--- c++_implementációk/Horspool.h
+++ c++_implementációk/Horspool.h
@@ -16,7 +16,39 @@ unordered_map<char, int> tav(string minta, string szoveg)
     }
     return E;
 }
-vector<int> Horspool(string minta, string szoveg, unordered_map<char, int> E)
+void val2(string abc, int *min, int *max)
+{
+    *min = abc[0];
+    *max = abc[0];
+    for (int i = 0; i < abc.length() - 1; i += 2)
+    {
+        if (abc[i] < *min)
+        {
+            *min = abc[i];
+        }
+
+        if (abc[i] > *max)
+        {
+            *max = abc[i];
+        }
+    }
+}
+vector<int> tav2(string minta, string szoveg, int *shift)
+{
+    int y;
+    val2(minta + szoveg, shift, &y);
+    vector<int> E = {};
+    for (int i = 0; i < *shift + y + 1; i++)
+    {
+        E.push_back(minta.length());
+    }
+    for (int i = 0; i < minta.length() - 1; i++)
+    {
+        E[minta[i] - *shift] = minta.length() - i - 1;
+    }
+    return E;
+}
+vector<int> Horspool(string minta, string szoveg, vector<int> E, int shift)
 {
     int j = minta.length() - 1;
     bool jo;
@@ -39,7 +71,7 @@ vector<int> Horspool(string minta, string szoveg, unordered_map<char, int> E)
                 talalatok.push_back(j - minta.length() + 1);
             }
         }
-        j = E[szoveg[j]] + j;
+        j = E[szoveg[j] - shift] + j;
     }
     return talalatok;
 }
