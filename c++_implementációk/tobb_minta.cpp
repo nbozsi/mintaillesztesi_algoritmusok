6577ad1fa011259d143117469aea815f9913cdff
nbozsi <nbozsi4@gmail.com>
2023-06-02 14:29:37 +0200

    javítás

--- c++_implementációk/tobb_minta.cpp
+++ c++_implementációk/tobb_minta.cpp
@@ -71,10 +71,11 @@ void timeit(vector<int> (*func)(string, string, vector<int>), vector<int> (*func
 {
     vector<int> pos;
     int talalat_counter = 0;
-    double min = 10000;
+    double sum = 0;
     for (int j = 0; j < tombmeret; j++)
     {
         vector<int> P = func2(minta[j]);
+        double min = 10000;
         for (int i = 0; i < n; i++)
         {
             auto t1 = high_resolution_clock::now();
@@ -87,18 +88,20 @@ void timeit(vector<int> (*func)(string, string, vector<int>), vector<int> (*func
                 min = ms_double.count();
             }
         }
+        sum = sum + min;
     }
-    cout << talalat_counter / 10 << '\t' << min;
+    cout << talalat_counter / 10 << '\t' << sum;
 }
 void timeit(vector<int> (*func)(string, string, int, int, long long int), void (*func2)(string, int *, int *), long long int (*func3)(string, int, int), string minta[], string szoveg, int tombmeret, int n)
 {
     vector<int> pos;
     int talalat_counter = 0;
-    double min = 10000;
+    double sum = 0;
     int x, y;
     long long int mintahash;
     for (int j = 0; j < tombmeret; j++)
     {
+        double min = 10000;
         func2(minta[j] + szoveg, &x, &y);
         mintahash = func3(minta[j], y, x);
         for (int i = 0; i < n; i++)
@@ -113,16 +116,18 @@ void timeit(vector<int> (*func)(string, string, int, int, long long int), void (
                 min = ms_double.count();
             }
         }
+        sum = sum + min;
     }
-    cout << talalat_counter / 10 << '\t' << min;
+    cout << talalat_counter / 10 << '\t' << sum;
 }
 void timeit(vector<int> (*func)(string, string, vector<int>, int), vector<int> (*func2)(string, string, int *), string minta[], string szoveg, int tombmeret, int n)
 {
     vector<int> pos;
     int talalat_counter = 0;
-    double min = 10000;
+    double sum = 0;
     for (int j = 0; j < tombmeret; j++)
     {
+        double min = 10000;
         int x;
         vector<int> tavok = func2(minta[j], szoveg, &x);
         for (int i = 0; i < n; i++)
@@ -137,16 +142,18 @@ void timeit(vector<int> (*func)(string, string, vector<int>, int), vector<int> (
                 min = ms_double.count();
             }
         }
+        sum = sum + min;
     }
-    cout << talalat_counter / 10 << '\t' << min;
+    cout << talalat_counter / 10 << '\t' << sum;
 }
 void timeit(vector<int> (*func)(string, string), string minta[], string szoveg, int tombmeret, int n)
 {
     vector<int> pos;
     int talalat_counter = 0;
-    double min = 10000;
+    double sum = 0;
     for (int j = 0; j < tombmeret; j++)
     {
+        double min = 10000;
         for (int i = 0; i < n; i++)
         {
             auto t1 = high_resolution_clock::now();
@@ -159,6 +166,7 @@ void timeit(vector<int> (*func)(string, string), string minta[], string szoveg, 
                 min = ms_double.count();
             }
         }
+        sum = sum + min;
     }
-    cout << talalat_counter / 10 << '\t' << min;
+    cout << talalat_counter / 10 << '\t' << sum;
 }
