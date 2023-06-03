05a0e656c248af1641bb15d36b3ac67e7073b437
nbozsi <nbozsi4@gmail.com>
2023-06-03 17:52:14 +0200

    brute froce osszehasonlitas és üres minta/szovegre nem fut le

--- c++_implementációk/k_diff_osszehasonlito.cpp
+++ c++_implementációk/k_diff_osszehasonlito.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Levenshtein_tav.h"
+#include "brute_force.h"
 #include <chrono>
 #include <string>
 
@@ -7,13 +8,27 @@ using namespace std;
 using namespace std::chrono;
 
 void timeit(vector<int> (*func)(string, string, int), string minta, string szoveg, int, int);
+void timeit(vector<int> (*func)(string, string), string minta, string szoveg, int);
 
 int main(int argc, char *argv[])
 {
-    string minta(argv[1]); // beolvasás
-    string szoveg(argv[2]);
-    int k = stoi(argv[3]);
+    string minta; // beolvasás
+    string szoveg;
+    int k;
+    if (argc > 1)
+    {
+        minta = argv[1]; // beolvasás
+        szoveg = argv[2];
+        k = stoi(argv[3]);
+    }
+    else
+    {
+        return 1;
+    }
     int probak = 5;
+    cout << "brute_force" << '\t';
+    timeit(&brute_force, minta, szoveg, probak);
+    cout << '\n';
 
     cout << "Dinamikus" << '\t';
     timeit(&Levenshtein_tavolsag, minta, szoveg, k, probak);
@@ -34,4 +49,21 @@ void timeit(vector<int> (*func)(string, string, int), string minta, string szove
         sum = sum + ms_double.count();
     }
     cout << pos.size() << '\t' << sum / n;
+}
+void timeit(vector<int> (*func)(string, string), string minta, string szoveg, int n)
+{
+    vector<int> pos;
+    double min = 1000;
+    for (int i = 0; i < n; i++)
+    {
+        auto t1 = high_resolution_clock::now();
+        pos = func(minta, szoveg);
+        auto t2 = high_resolution_clock::now();
+        duration<double, std::milli> ms_double = t2 - t1;
+        if (min > ms_double.count())
+        {
+            min = ms_double.count();
+        }
+    }
+    cout << pos.size() << '\t' << min;
 }
